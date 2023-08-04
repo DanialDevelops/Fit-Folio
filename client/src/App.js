69e2f7d81c1ab63942b97e3100425cf3e509d99d
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-08-03 21:10:30 -0400

    added css

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./pages/Home";
-import Exercise from "./components/exercise";
-import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
