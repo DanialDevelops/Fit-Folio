7c560deba537a1549584396716878549ed8ee060
Andre Besner <besner.andre@gmail.com>
2023-08-10 17:12:12 -0400

    add password information to server/models/user.js, add code to ensure password is new, unique and encrypted. also add code to ensure correct password based on previously hashed password

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema(
   {
@@ -19,6 +20,11 @@ const userSchema = new Schema(
           );
         },
       },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 5,
     },
     friends: [
       {
@@ -44,6 +50,19 @@ const userSchema = new Schema(
 userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
 });
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model("User", userSchema);
 
