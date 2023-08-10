0f86861501a93c6c6f70ae8c9efacd7c791a7194
DanialDevelops <danialchou101@gmail.com>
2023-08-10 19:31:56 -0400

    completed connection to mogoose models

--- client/src/pages/Exercise.js
+++ client/src/pages/Exercise.js
@@ -2,5 +2,15 @@ import React from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import Auth from "../utils/auth";
 import Header from "../components/header";
-import cardioIcon from "../assets";
-import resistanceIcon from "../assets";
+import cardioIcon from "../assets/cardio-icon.png"; // Update the paths accordingly
+import resistanceIcon from "../assets/resistance-icon.png"; // Update the paths accordingly
+import ExerciseCard from "../components/ExerciseCard"; // Import the ExerciseCard component
+
+export default function exercise() {
+
+  return (
+    <div className="homepage">
+      <ExerciseCard />
+    </div>
+  );
+}
--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,9 @@
 const User = require("./User");
 const Workout = require("./Workout");
-//const Cardio = require("./Cardio");
 const Sessions = require("./Sessions");
 
-module.exports = { User, Workout, Sessions };
+module.exports = {
+  User,
+  Workout,
+  Sessions,
+};
