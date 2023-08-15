118a81ed064efd4fb2c75e92287ceb860cd112de
Andre Besner <besner.andre@gmail.com>
2023-08-15 18:52:07 -0400

    made it to mutation aspect of type defs and am seeking a hand

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,21 +1,46 @@
-type User {
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql
+  type User {
     _id: ID!
     username: String!
     email: String!
-    password: String!
     cardioWorkouts: [CardioWorkout]
-    weightWorkous: [WeightWorkout]
-}
+    weightWorkouts: [WeightWorkout]
+  }
 
-type Auth {
+  type Auth {
     token: ID!
     user: User
-}
+  }
 
-type CardioWorkout {
+  type CardioWorkout {
     _id: ID!
     user: User!
     workoutName: String
-    distance: String 
+    distance: String
     date: String
-}
+  }
+
+  type WeightWorkout {
+    _id: ID!
+    user: User!
+    sets: String
+    reps: String
+    weight: String
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addCardioWorkout()
+  }
+;
+
+module.exports = typeDefs;
